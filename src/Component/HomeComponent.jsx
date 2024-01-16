b1e969c6f8db245a77160be10444e8a2b12b6356
Your Name~ <harish.s@google.com>
2024-01-16 16:12:45 +0530

    Complete

--- src/Component/HomeComponent.jsx
+++ src/Component/HomeComponent.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default class HomeComponent extends Component {
   render() {
     return (
-      <div>
+      <div id='div-container'>
         <h1 style={{color:'#fff'}}>Quiz App</h1>
         <button id="play">Play</button>
       </div>
